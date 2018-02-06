d406b25d0d0bfe3ef882176139a977064a4c66c6
astal <a.stal@voskhod.ru>
2018-02-06 15:27:43 +0300

    Костыльно вставлен vis.js и при инициализации отрисовывает полученные с rest граф

--- frontend/public/js/hello.js
+++ frontend/public/js/hello.js
@@ -1,15 +1,39 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
+app.controller('myCtrl', function($scope, $http, $element) {
     $scope.firstName= "John";
     $scope.lastName= "Doe";
     $scope.init = function () {
 
-    $http.get('http://localhost:9000/api/vertex').    
+        var nodesList = [];
+        var fromList = [];
+
+        $http.get('http://localhost:9000/api/vertex').
         then(function(response) {
-           alert(response.data);
+            var data = response.data;
+
+            for(var i in data)
+            {
+                var node = {id:data[i].id, label: "Node" + data[i].id, x:data[i].position.xposition, y:data[i].position.yposition};
+                var elementFrom = data[i].from;
+                for(var j in elementFrom){
+                    var currentFrom = {from:elementFrom[j].from, to:elementFrom[j].to};
+                    fromList.push(currentFrom);
+                }
+                nodesList.push(node);
+            }
+
+            var nodes = new vis.DataSet(nodesList);
+            var edges = new vis.DataSet(fromList);
+            var container =  document.querySelector('#mynetwork');
+            var data = {
+                nodes: nodes,
+                edges: edges
+            };
+            var options = {};
+            var network = new vis.Network(container, data, options);
         });
-    } 
-    
-    
+
+	}
+
 });
 
